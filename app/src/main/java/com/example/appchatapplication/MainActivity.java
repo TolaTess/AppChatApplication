241ab3ba48f84f6d0c5e031c62b6bbf3d18b07c3
Tola Tess <x17131553@student.ncirl.ie>
2020-05-18 21:48:17 +0100

    Registeration with firebase
    added registration layout and firebase authentication

--- app/src/main/java/com/example/appchatapplication/MainActivity.java
+++ app/src/main/java/com/example/appchatapplication/MainActivity.java
@@ -2,10 +2,12 @@ package com.example.appchatapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -13,12 +15,19 @@ import com.google.firebase.auth.FirebaseUser;
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         mAuth = FirebaseAuth.getInstance();
+
+        mToolbar = findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("App Chat");
+        mToolbar.bringToFront();
     }
 
     @Override
@@ -28,10 +37,34 @@ public class MainActivity extends AppCompatActivity {
         FirebaseUser currentUser = mAuth.getCurrentUser();
         if(currentUser == null)
         {
-            Intent intent = new Intent(this, StartActivity.class);
-            startActivity(intent);
-            //finish so user can't come back to this page until sign in
-            finish();
+            sendToStart();
         }
+    }
+
+    private void sendToStart() {
+        Intent intent = new Intent(this, StartActivity.class);
+        startActivity(intent);
+        //finish so user can't come back to this page until sign in
+        finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        getMenuInflater().inflate(R.menu.menu_item, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        if(item.getItemId() == R.id.logout_btn){
+            FirebaseAuth.getInstance().signOut();
+            sendToStart();
+        }
+
+        return true;
     }
 }
--- app/src/main/java/com/example/appchatapplication/RegisterActivity.java
+++ app/src/main/java/com/example/appchatapplication/RegisterActivity.java
@@ -1,15 +1,19 @@
 package com.example.appchatapplication;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -22,18 +26,29 @@ public class RegisterActivity extends AppCompatActivity {
     private static final String TAG = "RegisterActivity";
 
     private FirebaseAuth mAuth;
+    private Toolbar mToolbar;
 
     private TextInputLayout mDisplayName;
     private TextInputLayout mEmail;
     private TextInputLayout mPassword;
     private Button mCreateButton;
 
+    private ProgressDialog mRegProgress;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        mRegProgress = new ProgressDialog(this);
+
         mAuth = FirebaseAuth.getInstance();
+
+        mToolbar = findViewById(R.id.reg_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Create Account");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        mToolbar.bringToFront();
 
         mDisplayName = findViewById(R.id.reg_display_name);
         mEmail = findViewById(R.id.reg_email);
@@ -47,7 +62,16 @@ public class RegisterActivity extends AppCompatActivity {
                 String email = mEmail.getEditText().getText().toString();
                 String password = mPassword.getEditText().getText().toString();
 
-                register_user(display_name, email, password);
+                //check if information entered
+                if(!TextUtils.isEmpty(display_name) || !TextUtils.isEmpty(email) || !TextUtils.isEmpty(password)){
+
+                    mRegProgress.setTitle("Registering User");
+                    mRegProgress.setMessage("Please wait while we create your account");
+                    mRegProgress.setCanceledOnTouchOutside(false);
+                    mRegProgress.show();
+                    register_user(display_name, email, password);
+                }
+
             }
 
         });
@@ -58,13 +82,20 @@ public class RegisterActivity extends AppCompatActivity {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if(task.isSuccessful()){
-                    Intent mainIntent = new Intent(RegisterActivity.this, MainActivity.class);
-                    startActivity(mainIntent);
-                    finish();
+                    mRegProgress.dismiss();
+                    sendToStart();
                 } else{
-                    Toast.makeText(RegisterActivity.this, "Not Registered error", Toast.LENGTH_LONG).show();
+                    mRegProgress.hide();
+                    Toast.makeText(RegisterActivity.this, "An error occurred, please try again", Toast.LENGTH_LONG).show();
                 }
             }
         });
+    }
+
+    private void sendToStart() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        //finish so user can't come back to this page until sign in
+        finish();
     }
 }
