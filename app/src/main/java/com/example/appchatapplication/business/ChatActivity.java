369299aaba1ae2d8a80109e200c9eb8d54bc1aa1
Tola Tess <x17131553@student.ncirl.ie>
2020-05-23 18:02:35 +0100

    chat database
    created chat database to store chats  between users.

--- app/src/main/java/com/example/appchatapplication/business/ChatActivity.java
+++ app/src/main/java/com/example/appchatapplication/business/ChatActivity.java
@@ -1,22 +1,29 @@
 package com.example.appchatapplication.business;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
 
 import com.example.appchatapplication.R;
 import com.example.appchatapplication.utils.GetTimeAgo;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ServerValue;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -24,6 +31,8 @@ public class ChatActivity extends AppCompatActivity {
 
     private String mChatUser;
     private DatabaseReference mRootRef;
+    private FirebaseAuth mAuth;
+    private String mCurrentUserId;
     private Toolbar mToolbar;
 
     private CircleImageView mProfileImage;
@@ -40,6 +49,8 @@ public class ChatActivity extends AppCompatActivity {
         String userName = getIntent().getStringExtra("username");
 
         mRootRef = FirebaseDatabase.getInstance().getReference();
+        mAuth = FirebaseAuth.getInstance();
+        mCurrentUserId = mAuth.getCurrentUser().getUid();
 
 
         mToolbar = findViewById(R.id.chat_toolbar);
@@ -74,6 +85,35 @@ public class ChatActivity extends AppCompatActivity {
 
                     String lastSeenTime = getTimeAgo.getTimeAgo(lastTime, getApplicationContext());
                     mLastSeen.setText(lastSeenTime);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        mRootRef.child("Chat").child(mCurrentUserId).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(!dataSnapshot.hasChild(mChatUser)){
+                    Map chatAddMap = new HashMap();
+                    chatAddMap.put("seen", false);
+                    chatAddMap.put("timestamp", ServerValue.TIMESTAMP);
+
+                    Map chatUserMap = new HashMap();
+                    chatUserMap.put("Chat/" + mCurrentUserId + "/" + mChatUser, chatAddMap);
+                    chatUserMap.put("Chat/" + mChatUser + "/" + mCurrentUserId, chatAddMap);
+
+                    mRootRef.updateChildren(chatUserMap, new DatabaseReference.CompletionListener() {
+                        @Override
+                        public void onComplete(@Nullable DatabaseError databaseError, @NonNull DatabaseReference databaseReference) {
+                            if(databaseError != null ){
+                                Log.d("CHAT_LOG", databaseError.getMessage().toString());
+                            }
+                        }
+                    });
                 }
             }
 
