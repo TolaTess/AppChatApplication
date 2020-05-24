39acadf51b5f4f356d1a61c699abd1c411e9817c
Tola Tess <x17131553@student.ncirl.ie>
2020-05-24 21:38:29 +0100

    online tracking update
    added online tracking for all user's activity

--- app/src/main/java/com/example/appchatapplication/AllUserActivity.java
+++ app/src/main/java/com/example/appchatapplication/AllUserActivity.java
@@ -12,6 +12,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.appchatapplication.account.ProfileActivity;
@@ -19,9 +20,12 @@ import com.example.appchatapplication.model.Users;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ServerValue;
+import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
 import de.hdodenhof.circleimageview.CircleImageView;
@@ -34,6 +38,7 @@ public class AllUserActivity extends AppCompatActivity {
     private DatabaseReference mUserDatabase;
     private DatabaseReference mUserRef;
     FirebaseRecyclerAdapter adapter;
+    String currentuserId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +52,7 @@ public class AllUserActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        currentuserId = mAuth.getCurrentUser().getUid();
         mUserRef = FirebaseDatabase.getInstance()
                 .getReference().child("Users")
                 .child(mAuth.getCurrentUser().getUid());
@@ -76,9 +82,28 @@ public class AllUserActivity extends AppCompatActivity {
                 return new UsersViewHolder(view);
             }
             @Override
-            protected void onBindViewHolder(UsersViewHolder holder, int position, Users model) {
+            protected void onBindViewHolder(final UsersViewHolder holder, int position, Users model) {
                 Log.d(TAG, "onBindViewHolder: ");
-                holder.setName(model.getName());
+                final String list_user_id = getRef(position).getKey();
+                final String name = model.getName();
+
+                mUserDatabase.child(list_user_id).addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                        if(dataSnapshot.hasChild("online")){
+                            String userOnline = dataSnapshot.child("online").getValue().toString();
+                            holder.setUserOnline(userOnline);
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+
+                holder.setName(name);
                 holder.setStatus(model.getStatus());
                 holder.setImage(model.getThumb_image());
 
@@ -88,7 +113,8 @@ public class AllUserActivity extends AppCompatActivity {
                     @Override
                     public void onClick(View v) {
                         Intent profileIntent = new Intent(AllUserActivity.this, ProfileActivity.class);
-                        profileIntent.putExtra("user_id", userid); // send user id to use it to get all other info in db
+                        profileIntent.putExtra("user_id", userid);
+                        profileIntent.putExtra("username", name);// send user id to use it to get all other info in db
                         startActivity(profileIntent);
                     }
                 });
@@ -104,7 +130,6 @@ public class AllUserActivity extends AppCompatActivity {
         Log.d(TAG, "onStart: ");
         super.onStart();
         adapter.startListening();
-        mUserRef.child("online").setValue("true");
     }
 
     @Override
@@ -112,7 +137,6 @@ public class AllUserActivity extends AppCompatActivity {
         Log.d(TAG, "onStop: ");
         super.onStop();
         adapter.stopListening();
-        mUserRef.child("online").setValue(ServerValue.TIMESTAMP);
     }
 
     public static class UsersViewHolder extends RecyclerView.ViewHolder {
@@ -135,6 +159,17 @@ public class AllUserActivity extends AppCompatActivity {
         public void setImage(String thumb_image){
             CircleImageView mThumbImage = mView.findViewById(R.id.users_image);
             Picasso.get().load(thumb_image).placeholder(R.drawable.ic_launcher_foreground).into(mThumbImage);
+        }
+        public void setUserOnline(String online_status) {
+            ImageView userOnlineView = mView.findViewById(R.id.user_online_icon);
+
+            if(online_status.equals("true")){
+                userOnlineView.setVisibility(View.VISIBLE);
+
+            } else{
+                userOnlineView.setVisibility(View.INVISIBLE);
+
+            }
         }
     }
 }
