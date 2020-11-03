634db313ca6d8ae3328a559d01577bc6ffa8598c
Tola Tess <x17131553@student.ncirl.ie>
2020-11-03 22:39:54 +0000

    roulette wheel
    Added wheel to choose challenges at random

--- app/src/main/java/com/example/appchatapplication/fragment/challenges/ChallengesFragment.java
+++ app/src/main/java/com/example/appchatapplication/fragment/challenges/ChallengesFragment.java
@@ -5,9 +5,16 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RelativeLayout;
+import android.view.animation.Animation;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.android.volley.Request;
@@ -17,33 +24,111 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.appchatapplication.R;
-import com.example.appchatapplication.helpers.DialogFragmentHelper;
+import com.example.appchatapplication.modellayer.database.DatabasePresenter;
+import com.example.appchatapplication.modellayer.database.FirebaseDatabaseHelper;
 import com.example.appchatapplication.modellayer.model.GenerateActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.gson.Gson;
 
 import org.json.JSONObject;
 
+import java.util.Map;
+import java.util.Random;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
 public class ChallengesFragment extends Fragment {
     private RequestQueue requestQueue;
-    private TextView name;
+    private DatabasePresenter presenter;
+
+    @BindView(R.id.wheel)
+    ImageView wheel;
+    @BindView(R.id.indicator)
+    ImageView indicator;
+    @BindView(R.id.type_wheel)
+    TextView typeView;
+    @BindView(R.id.activity_wheel)
+    TextView challengeView;
+    @BindView(R.id.button_wheel)
+    Button acceptChallenge;
+
+    private Unbinder unbinder;
 
     public ChallengesFragment() {
         // Required empty public constructor
     }
 
+    @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_challenges, container, false);
+        unbinder = ButterKnife.bind(this, view);
+
+        wheel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                spinWheel(v);
+            }
+        });
 
         requestQueue = Volley.newRequestQueue(getContext());
-        name = view.findViewById(R.id.user_name);
-        defineView(view);
+        presenter = new FirebaseDatabaseHelper();
         return view;
     }
 
-    private void parseJason(final String activityType) {
-        final String URL = "http://www.boredapi.com/api/activity?type=" + activityType;
+    private void spinWheel(View view){
+        Random random = new Random();
+        final int degree = random.nextInt(360);
+
+        int endDegree = degree + 720;
+
+        RotateAnimation rotateAnimation = new RotateAnimation(0, endDegree,
+                RotateAnimation.RELATIVE_TO_SELF, 0.5f,
+                RotateAnimation.RELATIVE_TO_SELF, 0.5f);
+
+        rotateAnimation.setDuration(3000);
+        rotateAnimation.setFillAfter(true);
+        rotateAnimation.setInterpolator(new DecelerateInterpolator());
+
+        rotateAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                //calculateSpinEnd(degree);
+                displayChallenge();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        wheel.startAnimation(rotateAnimation);
+    }
+
+//    private void calculateSpinEnd(int degree){
+//        int initialPoint = 0;
+//        int endPoint = 30;
+//
+//        do{
+//            if(degree > initialPoint && degree < endPoint){
+//
+//            }
+//        }
+//    }
+
+
+    private void displayChallenge() {
+        final String URL = "http://www.boredapi.com/api/activity/";
         final JsonObjectRequest objectRequest = new JsonObjectRequest(
                 Request.Method.GET,
                 URL,
@@ -68,72 +153,41 @@ public class ChallengesFragment extends Fragment {
         Gson gson = new Gson();
         String result = response.toString();
         GenerateActivity activities = gson.fromJson(result, GenerateActivity.class);
-        String type = activities.getType();
-        String activity = activities.getActivity();
+        final String type = activities.getType();
+        final String activity = activities.getActivity();
 
-        DialogFragmentHelper fragmentHelper = new DialogFragmentHelper(type, activity);
-        fragmentHelper.show(getFragmentManager(), "DIALOG_FRAGMENT");
+        typeView.setText(type);
+        challengeView.setText(activity);
+
+        acceptChallenge.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "Challenge Accepted", Toast.LENGTH_SHORT).show();
+                Map setupMap = presenter.writeTodo(type, activity);
+                presenter.getmRootRef().updateChildren(setupMap).addOnCompleteListener(new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getContext(), "Saved in your challenge list", Toast.LENGTH_SHORT).show();
+                            acceptChallenge.setVisibility(View.INVISIBLE);
+                        } else
+                        {
+                            Toast.makeText(getContext(), "An Error Occurred while Posting", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
+
+        //DialogFragmentHelper fragmentHelper = new DialogFragmentHelper(type, activity);
+        //fragmentHelper.show(getFragmentManager(), "DIALOG_FRAGMENT");
     }
 
-    private void defineView(View view) {
-        RelativeLayout education, recreational, social, diy, charity, cooking, relaxation, music;
-        education = view.findViewById(R.id.education_rel);
-        education.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                parseJason("education");
-            }
-        });
 
-        recreational = view.findViewById(R.id.recre_rel);
-        recreational.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                parseJason("recreational");
-            }
-        });
-        social = view.findViewById(R.id.social_rel);
-        social.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                parseJason("social");
-            }
-        });
-        diy = view.findViewById(R.id.diy_rel);
-        diy.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                parseJason("diy");
-            }
-        });
-        charity = view.findViewById(R.id.charity_rel);
-        charity.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                parseJason("charity");
-            }
-        });
-        cooking = view.findViewById(R.id.cooking_rel);
-        cooking.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                parseJason("cooking");
-            }
-        });
-        relaxation = view.findViewById(R.id.relaxation_rel);
-        relaxation.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                parseJason("relaxation");
-            }
-        });
-        music = view.findViewById(R.id.music_rel);
-        music.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                parseJason("music");
-            }
-        });
-
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
     }
 }
